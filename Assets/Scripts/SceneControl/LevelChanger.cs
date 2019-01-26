6396fed306f94dd98bf522c6b3cc222020c01a91
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2019-01-26 15:14:01 -0500

    Added Scene Fade Transitions

---
+++ Assets/Scripts/SceneControl/LevelChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField]
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                FadeToLevel(0);
+            }
+            else
+            {
+                FadeToNextLevel();
+            }
+
+                
+            //FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
