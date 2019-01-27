f0e995e89dcd6c35d8473e4553b48eb20540f312
Brendan Nelligan <nelligan81@gmail.com>
2019-01-27 16:04:55 -0500

    Finished burnt house

---
+++ Assets/BurntHouseEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurntHouseEvent : LifeEvent
+{
+    public override void TriggerEvent()
+    {
+        if(CanTriggerEvent())
+        {
+            StartCoroutine(MournHouse());
+            base.TriggerEvent();
+        }
+    }
+    IEnumerator MournHouse()
+    {
+        stats.GetComponent<Animator>().SetTrigger("Cry");
+        yield return new WaitForSeconds(6);
+        FindObjectOfType<LevelChanger>().FadeToNextLevel();
+    }
+}
