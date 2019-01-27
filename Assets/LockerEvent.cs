3af83856416f03805e0e9171bff9966bf1a4b844
Brendan Nelligan <nelligan81@gmail.com>
2019-01-26 22:02:18 -0500

    Added books and lockers

---
+++ Assets/LockerEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerEvent : LifeEvent
+{
+    SpriteRenderer renderer;
+    private void Awake()
+    {
+        EventName = "Locker";
+        EventDescription = "Cool stuff in here...";
+        PrimaryStat = LifeStat.INTELLIGENCE;
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public override void TriggerEvent()
+    {
+        if(CanTriggerEvent())
+        {
+            if(renderer.enabled == false)
+            {
+                renderer.enabled = true;
+                Debug.Log("Locker opened!");
+            }
+            else
+            {
+                
+                base.TriggerEvent();
+            }
+        }
+    }
+
+}
