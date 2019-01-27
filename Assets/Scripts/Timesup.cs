3f4a50059ebd5dc67c804fab9c52914f658d413a
blaj95 <32546217+blaj95@users.noreply.github.com>
2019-01-27 12:28:30 -0500

    stage 2

---
+++ Assets/Scripts/Timesup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timesup : MonoBehaviour
+{
+    public float timer;
+
+    private bool called;
+
+    public LevelChanger levelChanger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 25 && !called)
+        {
+            //ring bell events
+            levelChanger.FadeToNextLevel();
+            called = true;
+        }
+    }
+}
